e1cf8d3833e7b3d9b9733f0278492c51f138cd48
jgongo <jose.gonzalez@openinput.com>
2013-06-15 16:33:03 +0200

    Added empty mapping category for book

---
+++ GGClient/GGClient/classes/helpers/mappings/GGBook+RKMapping.m
@@ -0,0 +1,18 @@
+//
+//  GGBook+RKMapping.m
+//  GGClient
+//
+//  Created by Jose Gonzalez Gomez on 15/06/13.
+//  Copyright (c) 2013 OPEN input. All rights reserved.
+//
+
+#import "GGBook+RKMapping.h"
+
+
+@implementation GGBook (RKMapping)
+
++ (RKMapping *)mapping {
+    return nil;
+}
+
+@end
