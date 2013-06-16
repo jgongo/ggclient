f586c0289c807dcf2249c9b22ae652f052729e56
jgongo <jose.gonzalez@openinput.com>
2013-06-16 19:26:46 +0200

    Pagination implementation

--- GGClient/GGClient/classes/controllers/GGCatalogViewController.m
+++ GGClient/GGClient/classes/controllers/GGCatalogViewController.m
@@ -18,6 +18,8 @@
 @property (nonatomic, strong) GGCatalogPageRetriever *paginator;
 @property (nonatomic, strong) id<INPaginationState>   paginationState;
 @property (nonatomic, strong) NSMutableArray         *books;
+
+@property (nonatomic, getter = isActivityIndicatorVisible) BOOL activityIndicatorVisible;
 @end
 
 
@@ -38,26 +40,51 @@
     }];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Page retrieval
+
+- (NSArray *)indexPathsFromRow:(NSUInteger)firstRow toRow:(NSUInteger)lastRow {
+    NSMutableArray *indexPaths = [[NSMutableArray alloc] init];
+    for (NSUInteger row = firstRow; row < lastRow; row++) {
+        [indexPaths addObject:[NSIndexPath indexPathForRow:row inSection:0]];
+    }
+    return indexPaths;
+}
+
+- (void)retrieveNextPageOfCatalog {
+    self.activityIndicatorVisible = YES;
+    [self.tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:self.books.count inSection:0]] withRowAnimation:UITableViewRowAnimationFade];
+    [self.paginator retrieveNextPageFrom:self.paginationState onSuccess:^(NSArray *page, id<INPaginationState> paginationState) {
+        self.paginationState = paginationState;
+        self.activityIndicatorVisible = NO;
+        [self.tableView deleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:self.books.count inSection:0]] withRowAnimation:UITableViewRowAnimationFade];
+        [self.books addObjectsFromArray:page];
+        [self.tableView insertRowsAtIndexPaths:[self indexPathsFromRow:self.books.count - page.count toRow:self.books.count] withRowAnimation:UITableViewRowAnimationFade];
+    } onError:^(NSError *error) {
+        self.activityIndicatorVisible = NO;
+        [self.tableView deleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:self.books.count inSection:0]] withRowAnimation:UITableViewRowAnimationFade];
+    }];
 }
 
 #pragma mark - Table View
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.books.count;
+    return self.books.count + (self.isActivityIndicatorVisible ? 1 : 0);
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"BOOK_CELL" forIndexPath:indexPath];
-
-    GGBook *book = self.books[indexPath.row];
-    cell.textLabel.text = book.title;
-    return cell;
+    if (indexPath.row == self.books.count - 1 && [self.paginationState hasMorePages]) {
+        [self performSelector:@selector(retrieveNextPageOfCatalog) withObject:nil afterDelay:0.1];
+    }
+    if (indexPath.row < self.books.count) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"BOOK_CELL" forIndexPath:indexPath];
+        GGBook *book = self.books[indexPath.row];
+        cell.textLabel.text = book.title;
+        return cell;
+    } else {
+        return [self.tableView dequeueReusableCellWithIdentifier:@"ACTIVITY_CELL" forIndexPath:indexPath];
+    }
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
